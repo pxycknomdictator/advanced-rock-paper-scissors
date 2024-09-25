df550f363b65b826ab519e4013bf837e823477ec
Noman <pxycknomdictator@outlook.com>
2024-09-25 16:03:02 +0500

    added user score logic

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import scores from "/images/logo.svg";
+import { gameContext } from "../store/gameStore";
 
 export const Scoreboard = () => {
+  const { score } = useContext(gameContext);
+
   return (
     <section className="flex items-center justify-between w-[85%] mx-auto md:w-[75%] xl:w-[65%] border-[3px] rounded-lg py-2.5 px-3 border-headerOutline">
       <div>
@@ -8,7 +12,7 @@ export const Scoreboard = () => {
       </div>
       <div className="bg-white py-4 px-9 smd:py-2.5 sm:px-10 rounded-md text-center -space-y-1">
         <span className="uppercase text-scoreText font-bold">score</span>
-        <p className="text-4xl sm:text-5xl font-bold text-darkText">0</p>
+        <p className="text-4xl sm:text-5xl font-bold text-darkText">{score}</p>
       </div>
     </section>
   );
--- src/store/gameStoreProvider.jsx
+++ src/store/gameStoreProvider.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import { gameContext } from "./gameStore.js";
 
 // eslint-disable-next-line react/prop-types
 export const GameContextProvider = ({ children }) => {
-  
+  const [score, setScore] = useState(0);
+
+  const compare = (user, computer) => {
+    if (
+      (user === "rock" && computer === "scissors") ||
+      (user === "paper" && computer === "rock") ||
+      (user === "scissors" && computer === "paper")
+    ) {
+      setScore((preScore) => preScore + 1);
+      console.log("You win");
+    } else if (computer === user) {
+      console.log("Draw Game");
+    } else {
+      console.log("Computer Win");
+    }
+  };
+
   const computer_Choice = () => {
     const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * 2);
@@ -11,12 +28,11 @@ export const GameContextProvider = ({ children }) => {
 
   const user_Choice = (userChoice) => {
     const computerChoice = computer_Choice();
-    console.log("User Choice", userChoice);
-    console.log("Computer Choice", computerChoice);
+    compare(userChoice, computerChoice);
   };
 
   return (
-    <gameContext.Provider value={{ user_Choice }}>
+    <gameContext.Provider value={{ user_Choice, score }}>
       {children}
     </gameContext.Provider>
   );
