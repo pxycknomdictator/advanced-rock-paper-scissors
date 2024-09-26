3b821b5471690d6e7fd347b78e1484a99cc16831
Noman <pxycknomdictator@outlook.com>
2024-09-27 00:49:14 +0500

    added advanced score board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Game } from "./components/Game";
-import { Scoreboard } from "./components/Scoreboard";
+// import { Scoreboard } from "./components/Scoreboard";
+import { AdvancedScoreboard } from "./components/AdvancedScoreboard";
 import { RuleBtn } from "./components/RuleBtn";
 
 export const App = () => {
   return (
     <main className="w-full relative min-h-screen bg-gradient-to-l to-radialBackground from-radialBackground-light pt-7">
-      <Scoreboard />
+      {/* <Scoreboard /> */}
+      <AdvancedScoreboard />
       <Game />
       <div className="text-center md:text-right px-6 mt-10 md:mt-0 absolute bottom-5 right-0">
         <RuleBtn />
---
+++ src/components/AdvancedScoreboard.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import scores from "/images/logo-bonus.svg";
+import { gameContext } from "../store/gameStore";
+
+export const AdvancedScoreboard = () => {
+  const { score } = useContext(gameContext);
+
+  return (
+    <section className="flex items-center justify-between w-[85%] mx-auto md:w-[75%] xl:w-[65%] border-[3px] rounded-lg py-2.5 px-3 border-headerOutline">
+      <div>
+        <img className="w-[60%] sm:w-full" src={scores} alt="logo" />
+      </div>
+      <div className="bg-white py-4 px-9 smd:py-2.5 sm:px-10 rounded-md text-center -space-y-1">
+        <span className="uppercase text-scoreText font-bold">score</span>
+        <p className="text-4xl sm:text-5xl font-bold text-darkText">{score}</p>
+      </div>
+    </section>
+  );
+};
